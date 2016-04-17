96bd333e342c24b7aff48a5b220cf87caf377767
Carlie Duncan <duncan.carlie@gmail.com>
2016-04-17 16:51:03 -0600

    Adding styling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-
-
+var isDraggable = !('ontouchstart' in document.documentElement);
+var mapOptions = {
+  draggable: isDraggable,
+  scrollwheel: false
+};
 
 // NOTE: closes menu on mobile
 $('.nav a').on('click', function(){
